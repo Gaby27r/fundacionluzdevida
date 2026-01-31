45a5e620bc127a02b64f2a6b43b79df06fbaa560
Gaby27r <gabriela-rojas@javeriana.edu.co>
2026-01-31 13:39:25 -0500

    Added Favicon

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import faviconClaro from './assets/logos/icono_favicon_claro.ico?url';
+
+const link = document.querySelector('link[rel="icon"]') as HTMLLinkElement | null;
+if (link) link.href = faviconClaro;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
